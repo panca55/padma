7ee51ac304511e5d6e5cc611f87f25a772f07089
panca55 <pancamember@gmail.com>
2025-12-02 01:18:57 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: './',
 })
